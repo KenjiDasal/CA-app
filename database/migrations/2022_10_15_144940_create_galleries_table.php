afaea268b5889c3b3e003332006e6671418cc93c
Kenji Dasal <35777115+KenjiDasal@users.noreply.github.com>
2022-10-15 16:26:55 +0100

    Database Updated

--- database/migrations/2022_10_15_144940_create_galleries_table.php
+++ database/migrations/2022_10_15_144940_create_galleries_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('galleries', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('artist');
+            $table->text('description');
+            $table->integer('likes');
             $table->timestamps();
         });
     }
